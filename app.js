6936a2f614af5e93dcea45d0cfbc112cb9429cf9
purbon <pere.urbon@gmail.com>
2013-06-12 14:37:34 +0200

    remove the default redirect to the main page

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 (function() {
   var lang = window.navigator.userLanguage || window.navigator.language;
   var url = "index.ca.html"
-  if (lang == "en-US") {
-    url = "http://doitreatthisimmigrant.com/";
-  } else if (lang == "es-ES") {
+  if (lang == "es-ES") {
     url = "index.es.html";
   }
   window.location = url
